4cb5f363baddc89789b3fdfb314694be50a1fa6b
Bineshbkp <161809098+Bineshbkp@users.noreply.github.com>
2026-01-16 21:30:33 +0530

    Delete src/pages/employee directory

--- src/pages/employee/selectors.js
+++
@@ -1,8 +0,0 @@
-import {flow} from 'lodash';
-
-const getCount=(state) => state['counter'];
-
-const selectApiData=(state)=>state.apiData;
-export const getSelectApiData=flow(getCount,selectApiData)
-
-
